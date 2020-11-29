d561b8136d4eefac4e4b16a1fa2ffb17347cbec9
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-29 23:08:57 +0700

    Create Latihan3Prak8.py

---
+++ Latihan3Prak8.py
@@ -0,0 +1,12 @@
+a=int(input('Mau input berapa nama? '))
+l=[]
+i=0
+for i in range (a):
+    nama=input('Masukkan Nama = ' )
+    l.append(nama)
+l.sort()
+
+r=0
+for r in range(len(l)) :
+    print(l[r], '(',len(l[r]),'karakter)')
+    r+=1
