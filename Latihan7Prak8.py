bcb39fb998370e0f979cdddbd3f1a5cd757f743e
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-29 23:09:07 +0700

    Create Latihan7Prak8.py

---
+++ Latihan7Prak8.py
@@ -0,0 +1,17 @@
+daftarbuah = {
+    'Apel'  : 5000,
+    'Jeruk' : 8500,
+    'Mangga': 7800,
+    'Duku'  : 6500
+}
+
+def mostExpensive(dictionary) :
+    
+    key = list(dictionary.keys())
+    values = tuple(dictionary.values())
+    hargaExpensive = max(values)
+    indexHargaExpensive = values.index(hargaExpensive)
+    print(key[indexHargaExpensive])
+    
+print('Harga buah yang paling mahal adalah :')
+mostExpensive(daftarbuah)
