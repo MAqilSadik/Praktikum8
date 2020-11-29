4e118f6d695a5646576f97a4062e9fc1c08cb4b0
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-29 23:09:05 +0700

    Create Latihan6Prak8.py

---
+++ Latihan6Prak8.py
@@ -0,0 +1,7 @@
+def sortStringByChar(data) :
+    data.sort(reverse = True)
+    return data
+data = ['apel','rambutan','jeruk']
+dataBaruSorted = sortStringByChar(data)
+
+print(sortStringByChar(data))
